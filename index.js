8f6ab517476ed4666d4052bd2a645090e8199282
Geoff Kelly <geoffkellync@gmail.com>
2023-10-22 17:32:47 -0400

    added changes to index.js

--- index.js
+++ index.js
@@ -2,13 +2,13 @@ require("dotenv").config();
 const server = require('./server')
 
 
-const PORT = process.env.PORT || 9001;
 
 
 
 
 
-server.listen(PORT, () => {
+
+server.listen(process.env.PORT, () => {
     console.log(`Server is running on port: ${PORT}....`);
   });
 
