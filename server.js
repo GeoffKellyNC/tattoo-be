dba8723192492e413005467da024100240ecb53e
Geoff Kelly <geoffkellync@gmail.com>
2023-10-22 17:53:50 -0400

    updated server file

--- server.js
+++ server.js
@@ -33,7 +33,7 @@ server.use('/auth', require('./routes/auth'))
 server.use('/user', require('./routes/user'))
 
 
-const PORT = process.env.port || 9001;
+const PORT = process.env.PORT || 9001;
 server.listen(PORT, () => {
     console.log(`Server is running on port: ${PORT}....`);
   });
