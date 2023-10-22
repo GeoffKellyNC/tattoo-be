95f5dc0cf3dff0f0deca1ffb66b4d9b1691dde1c
Geoff Kelly <geoffkellync@gmail.com>
2023-10-22 17:46:49 -0400

    added procfile for heroku

--- index.js
+++ index.js
@@ -2,13 +2,13 @@ require("dotenv").config();
 const server = require('./server')
 
 
+const PORT = process.env.port || 9001;
 
 
 
 
 
-
-server.listen(process.env.PORT, () => {
+server.listen(PORT, () => {
     console.log(`Server is running on port: ${PORT}....`);
   });
 
