1990a4e60dd079ff178b41e263f744ee3dbe1609
Geoff Kelly <geoffkellync@gmail.com>
2024-01-06 16:04:22 -0500

    added addional console.logs

--- util/customConsole.js
+++ util/customConsole.js
@@ -4,10 +4,10 @@
 const customConsole = (data) => {
     const formatedDate = new Date(Date.now()).toLocaleString('en-US', { timeZone: 'America/New_York' });
     console.log(`
-        User: ${data.unxid ? data.unxid : 'No unxid'}
-        Current time: ${formatedDate}
-        req.path: ${data.path}
-        metaData: ${data.metaData ? data.metaData : 'No metaData'}
+        👤 User: ${data.unxid ? data.unxid : 'No unxid'}
+        ⏰ Current time: ${formatedDate}
+        🦠 req.path: ${data.path}
+        📀 metaData: ${data.metaData ? data.metaData : 'No metaData'}
 
     `)
 }
